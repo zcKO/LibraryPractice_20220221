81f6b4f2ecf25ef5046293770d661e501108ff98
JaeCheon <studyJcHub@gmail.com>
2022-02-21 11:52:26 +0900

    사진 크게 보기 화면 추가  - 라이브러리는 활용하지 않았다.

--- app/src/main/java/com/jc/librarypractice_20220221/MainActivity.kt
+++ app/src/main/java/com/jc/librarypractice_20220221/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.jc.librarypractice_20220221
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpEvents()
+        setValues()
+
     }
+
+    fun setUpEvents() {
+
+//        버튼 이외의, TextView, ImageView, LinearLayout 등등 setOnClickListener 로 이벤트 처리가 가능하다.
+        imgProfile.setOnClickListener {
+//            사진을 크게 보는 액티비티로 이동
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+        }
+
+    }
+
+    fun setValues() {
+
+    }
+
 }
---
+++ app/src/main/java/com/jc/librarypractice_20220221/ViewPhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.jc.librarypractice_20220221
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo)
+    }
+}
