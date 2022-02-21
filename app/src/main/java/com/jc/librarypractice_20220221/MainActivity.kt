351671f3145348dfb0f7cc3463e8a42f584a1644
JaeCheon <studyJcHub@gmail.com>
2022-02-21 12:12:27 +0900

    CALL 기능 실행  - 권한 문제로 앱이 죽는다.

--- app/src/main/java/com/jc/librarypractice_20220221/MainActivity.kt
+++ app/src/main/java/com/jc/librarypractice_20220221/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.jc.librarypractice_20220221
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,6 +17,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setUpEvents() {
+
+//        전화걸기
+        btnCall.setOnClickListener {
+            val myUri = Uri.parse("tel:0103333777")
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+        }
+
 
 //        버튼 이외의, TextView, ImageView, LinearLayout 등등 setOnClickListener 로 이벤트 처리가 가능하다.
         imgProfile.setOnClickListener {
