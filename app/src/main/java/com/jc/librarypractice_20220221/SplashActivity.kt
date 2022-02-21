0f0485a9971dd7ad9de01b25e737100bd3b8a8e2
JaeCheon <studyJcHub@gmail.com>
2022-02-21 16:32:41 +0900

    App 의 시작 화면 변경  - SplashActivity 로 변경

---
+++ app/src/main/java/com/jc/librarypractice_20220221/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.jc.librarypractice_20220221
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
