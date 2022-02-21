52704af3dab09db3e15b3b17c21b6bd4b702f74e
JaeCheon <studyJcHub@gmail.com>
2022-02-21 16:37:55 +0900

    2.5 초 후에 메인화면으로 이동

--- app/src/main/java/com/jc/librarypractice_20220221/SplashActivity.kt
+++ app/src/main/java/com/jc/librarypractice_20220221/SplashActivity.kt
@@ -1,11 +1,23 @@
 package com.jc.librarypractice_20220221
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 2500)
+
     }
 }
