96a5cf640604a024264721e8a981f4ac02b69034
JaeCheon <studyJcHub@gmail.com>
2022-02-21 14:51:33 +0900

    TedPermission 라이브러리 설치, PermissionListener 를 이용해서 - 승인 시 / 거부시의 대응방안 미리 작성

--- app/src/main/java/com/jc/librarypractice_20220221/MainActivity.kt
+++ app/src/main/java/com/jc/librarypractice_20220221/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.jc.librarypractice_20220221
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,9 +25,8 @@ class MainActivity : AppCompatActivity() {
 
 //        전화걸기
         btnCall.setOnClickListener {
-            val myUri = Uri.parse("tel:0103333777")
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+
+
         }
 
 
@@ -38,5 +42,26 @@ class MainActivity : AppCompatActivity() {
     fun setValues() {
 
     }
+
+    fun getPermissionListener() {
+//        권한 승인 여부에 따른, 행동 방안을 작성해서 pl 변수에 담아 둔다.
+        val pl = object : PermissionListener {
+            override fun onPermissionGranted() {
+//                승인이 OK 일 때 할 행동
+                val myUri = Uri.parse("tel:0103333777")
+                val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                startActivity(myIntent)
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                거절 되었을 때 할 행동
+                Toast.makeText(this@MainActivity, "권한이 거절되어, 통화가 불가능 합니다.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
+
+    }
+
 
 }
